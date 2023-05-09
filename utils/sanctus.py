f498b8e5761b2217e5081f010414430dfe88ec0d
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-05-10 00:06:35 +0100

    patch

--- utils/sanctus.py
+++ utils/sanctus.py
@@ -8,20 +8,23 @@ from utils.general import ( xyxy2xywh)
 class Processor:
       
       def obj_class(names_class="Don't spray", obj_data_als=[],im0=[],n='',time_set='5s'):
-            GPIO.setmode(GPIO.MODE)
-            GPIO.setup(18,GPIO.OUT,initial=GPIO.LOW)
+            # GPIO.setmode(GPIO.MODE)
+            # GPIO.setup(18,GPIO.OUT,initial=GPIO.LOW)
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]] 
-            for *xyxy, conf, cls in obj_data_als:
-                  if str(names_class)==str('person'):  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist() #Getting the current width,hight and xy cord.add
-                        #print(conf) #this is the confidence level
-                        # print(cls) #this is the given class of the object detected
-                        GPIO.output(18,GPIO.HIGH)
-                        time.sleep(time)
-                        print ('spraying completed')
-                        GPIO.cleanup()
-
-      def cal_ord_area(xywh):
-                                    #area calculation function
-       print(xywh,'cal_ord_area')
+            if str(names_class)==str('person'):
+                  total_area=0
+                  for *xyxy, conf, cls in obj_data_als:
+                        co_ordinate = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()
+                        x,y, width ,hieght = co_ordinate
+                        area = width * hieght
+                        total_area+= area
+                  if True:
+                        print(total_area,'out of lope')
+    
+            else:
+                  print("Warning: the class is not defined")
        
+                        # print(cls) #this is the given class of the object detected
+                        # GPIO.output(18,GPIO.HIGH)
+                        # time.sleep(time)
+                        # GPIO.cleanup()
