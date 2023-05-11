f8171fb1d382e004bb742788ee9226f10f3bb7d6
sherlockholms221B <chineduchristsanctus020@gmail.com>
2023-05-11 13:16:32 +0100

    patch

--- utils/sanctus.py
+++ utils/sanctus.py
@@ -1,7 +1,7 @@
 import torch
 import time
 import math
-import Jetson.GPIO as GPIO
+# import Jetson.GPIO as GPIO
 
 from utils.general import ( xyxy2xywh)
 
@@ -9,8 +9,8 @@ from utils.general import ( xyxy2xywh)
 class Processor:
       
       def obj_class(names_class="Don't spray", obj_data_als=[],im0=[],n='',time_set='5s'):
-            GPIO.setmode(GPIO.MODE)
-            GPIO.setup(18,GPIO.OUT,initial=GPIO.LOW)
+            # GPIO.setmode(GPIO.MODE)
+            # GPIO.setup(18,GPIO.OUT,initial=GPIO.LOW)
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]] 
             if str(names_class)==str('cassava'):
                   total_area=0
@@ -23,29 +23,29 @@ class Processor:
                   
                   if per_cent_csl < 5:
                         print(str(per_cent_csl)+'%')
-                        GPIO.output(18,GPIO.HIGH)
+                        # GPIO.output(18,GPIO.HIGH)
                         time.sleep(per_cent_csl)
-                        GPIO.cleanup()
+                        # GPIO.cleanup()
                   elif per_cent_csl >= 5 and per_cent_csl < 10:
                         print(str(per_cent_csl)+'%')
-                        GPIO.output(18,GPIO.HIGH)
+                        # GPIO.output(18,GPIO.HIGH)
                         time.sleep(per_cent_csl)
-                        GPIO.cleanup()
+                        # GPIO.cleanup()
                   elif per_cent_csl >= 10 and per_cent_csl <20:
                         print(str(per_cent_csl)+'%')
-                        GPIO.output(18,GPIO.HIGH)
+                        # GPIO.output(18,GPIO.HIGH)
                         time.sleep(per_cent_csl)
-                        GPIO.cleanup()
+                        # GPIO.cleanup()
                   elif per_cent_csl >= 20 and per_cent_csl < 30:
                         print(str(per_cent_csl)+'%')
-                        GPIO.output(18,GPIO.HIGH)
+                        # GPIO.output(18,GPIO.HIGH)
                         time.sleep(per_cent_csl)
-                        GPIO.cleanup()
+                        # GPIO.cleanup()
                   else :
                         print(str(per_cent_csl)+'%')
-                        GPIO.output(18,GPIO.HIGH)
+                        # GPIO.output(18,GPIO.HIGH)
                         time.sleep(time)
-                        GPIO.cleanup()
+                        # GPIO.cleanup()
     
             else:
                   print("Warning: the class is not defined")
